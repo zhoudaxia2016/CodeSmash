02c1b8c02413b668279c8c173de8ba40417f7a02
zhoudaxia <muzhouz@gmail.com>
2026-04-05 21:03:02 +0800

    feat: 调整前端base路径

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/codesmesh/' : '/',
   plugins: [react()],
   assetsInclude: ['**/*.wasm'],
   optimizeDeps: {
@@ -39,4 +40,4 @@ export default defineConfig({
       },
     },
   },
-})
+}))
